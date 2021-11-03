be711c0ee6058e9aa76d54061930db1eedc506de
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2021-11-03 19:27:32 +0100

    build: introduce typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     commonjs: true,
@@ -6,11 +7,17 @@ module.exports = {
     node: true,
     "jest/globals": true,
   },
-  extends: ["airbnb-base", "prettier"],
+  extends: [
+    "airbnb-base",
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
   },
-  plugins: ["jest"],
+  plugins: ["jest", "@typescript-eslint"],
   rules: {
     quotes: 0,
     "no-plusplus": "off",
@@ -19,5 +26,6 @@ module.exports = {
     "no-param-reassign": 1,
     "implicit-arrow-linebreak": "off",
     "function-paren-newline": "off",
+    "@typescript-eslint/no-var-requires": 0,
   },
 };
--- example/solution.ts
+++ example/solution.ts
@@ -1,4 +1,4 @@
-const sum = (a, b) => a + b;
+const sum = (a: number, b): number => a + b;
 
 console.log(sum(4, 6));
 module.exports = sum;
