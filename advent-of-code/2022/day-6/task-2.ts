8d4bca798adae55b93a885fd96e87f3003cda6e0
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2022-12-06 18:54:48 +0100

    feat(aoc): add solutions 1 and 2 to the Advent of Code: Day 6 🧝‍♀️

---
+++ advent-of-code/2022/day-6/task-2.ts
@@ -0,0 +1,26 @@
+/* --- Part Two ---
+Your device's communication system is correctly detecting packets, but still isn't working. It looks like it also needs to look for messages.
+
+A start-of-message marker is just like a start-of-packet marker, except it consists of 14 distinct characters rather than 4.
+
+Here are the first positions of start-of-message markers for all of the above examples:
+
+mjqjpqmgbljsphdztnvjfqwrcgsmlb: first marker after character 19
+bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 23
+nppdvjthqldpwncqszvftbrmjlhg: first marker after character 23
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 29
+zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 26
+How many characters need to be processed before the first start-of-message marker is detected?
+*/
+import findUniqueSequence from "./task-1";
+
+export {};
+const fs = require("fs");
+
+const dataset: string = fs.readFileSync(
+  "advent-of-code/2022/day-6/data.txt",
+  "utf8",
+  () => null
+);
+
+console.log(findUniqueSequence(dataset, 14));
