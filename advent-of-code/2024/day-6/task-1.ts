1514dbb5808d5be5128ddd210ce289b3115294d7
Vladlena Panchenko <panchenko.vladlena@gmail.com>
2024-12-06 13:51:23 +0100

    feat(aoc): add solution 1 to the Advent of Code: Day 6 🌟

---
+++ advent-of-code/2024/day-6/task-1.ts
@@ -0,0 +1,185 @@
+/* --- Day 6: Guard Gallivant ---
+The Historians use their fancy device again, this time to whisk you all away to the North Pole prototype suit manufacturing lab... in the year 1518! It turns out that having direct access to history is very convenient for a group of historians.
+
+You still have to be careful of time paradoxes, and so it will be important to avoid anyone from 1518 while The Historians search for the Chief. Unfortunately, a single guard is patrolling this part of the lab.
+
+Maybe you can work out where the guard will go ahead of time so that The Historians can search safely?
+
+You start by making a map (your puzzle input) of the situation. For example:
+
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+The map shows the current position of the guard with ^ (to indicate the guard is currently facing up from the perspective of the map). Any obstructions - crates, desks, alchemical reactors, etc. - are shown as #.
+
+Lab guards in 1518 follow a very strict patrol protocol which involves repeatedly following these steps:
+
+If there is something directly in front of you, turn right 90 degrees.
+Otherwise, take a step forward.
+Following the above protocol, the guard moves up several times until she reaches an obstacle (in this case, a pile of failed suit prototypes):
+
+....#.....
+....^....#
+..........
+..#.......
+.......#..
+..........
+.#........
+........#.
+#.........
+......#...
+Because there is now an obstacle in front of the guard, she turns right before continuing straight in her new facing direction:
+
+....#.....
+........>#
+..........
+..#.......
+.......#..
+..........
+.#........
+........#.
+#.........
+......#...
+Reaching another obstacle (a spool of several very long polymers), she turns right again and continues downward:
+
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#......v.
+........#.
+#.........
+......#...
+This process continues for a while, but the guard eventually leaves the mapped area (after walking past a tank of universal solvent):
+
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#........
+........#.
+#.........
+......#v..
+By predicting the guard's route, you can determine which specific positions in the lab will be in the patrol path. Including the guard's starting position, the positions visited by the guard before leaving the area are marked with an X:
+
+....#.....
+....XXXXX#
+....X...X.
+..#.X...X.
+..XXXXX#X.
+..X.X.X.X.
+.#XXXXXXX.
+.XXXXXXX#.
+#XXXXXXX..
+......#X..
+In this example, the guard will visit 41 distinct positions on your map.
+
+Predict the path of the guard. How many distinct positions will the guard visit before leaving the mapped area?
+*/
+export {};
+const fs = require("fs");
+
+const dataset: string = fs.readFileSync(
+  "advent-of-code/2024/day-6/data.txt",
+  "utf8",
+  () => null
+);
+
+let guard = null;
+
+const input = dataset.split("\n").map((line, i) => {
+  const split = line.split("");
+
+  if (!guard) {
+    const guardLocation = split.findIndex((item) => item === "^");
+    if (guardLocation >= 0) guard = [i, guardLocation];
+  }
+
+  return split;
+});
+
+enum Direction {
+  up = "up",
+  down = "down",
+  left = "left",
+  right = "right",
+}
+
+const DIRECTIONS_MAP = ["up", "right", "down", "left"];
+
+const findIndex = ({ cx, cy, direction }) => {
+  switch (direction) {
+    case Direction.up:
+      return [cx - 1, cy];
+    case Direction.right:
+      return [cx, cy + 1];
+    case Direction.down:
+      return [cx + 1, cy];
+    case Direction.left:
+    default:
+      return [cx, cy - 1];
+  }
+};
+
+const makeStep = ({ cx, cy, direction }) => {
+  let dir = direction;
+  const [newX, newY] = findIndex({ cx, cy, direction });
+  let x = newX;
+  let y = newY;
+
+  if (!input[x] || !input[x][y]) {
+    return false;
+  }
+  if (input[x][y] === "#") {
+    dir =
+      DIRECTIONS_MAP[
+        DIRECTIONS_MAP.findIndex((item) => item === direction) + 1
+      ] ?? DIRECTIONS_MAP[0];
+
+    x = cx;
+    y = cy;
+  }
+
+  return { x, y, direction: dir };
+};
+
+const findVisitedTiles = () => {
+  let res = 1;
+
+  const history = [`${guard[0]};${guard[1]}`];
+  let cx = guard[0];
+  let cy = guard[1];
+  let dir = Direction.up;
+
+  while (true) {
+    const newLoc = makeStep({ cx, cy, direction: dir });
+
+    if (newLoc) {
+      const { x, y, direction } = newLoc;
+      cx = x;
+      cy = y;
+      dir = direction;
+      if (!history.includes(`${x};${y}`)) {
+        res++;
+        history.push(`${x};${y}`);
+      }
+
+      makeStep({ cx: x, cy: y, direction });
+    } else break;
+  }
+
+  return res;
+};
+
+console.log(findVisitedTiles());
